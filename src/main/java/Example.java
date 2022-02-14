25c6e5d01e48d1c82603a09aae8c0db424783641
naraspai <naraspai@yahoo.co.in>
2022-02-15 00:28:11 +0530

    added Math.random()

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -2,6 +2,7 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+import java.lang.Math;
 
 @RestController
 @EnableAutoConfiguration
